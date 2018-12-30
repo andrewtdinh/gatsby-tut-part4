00b2f79e40ddabec078e0d9e0f947c0293ee1cde
Andrew Dinh <adinh@sightmachine.com>
2018-12-29 23:49:12 -0800

    Add links to the pages on the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,40 @@
 import React from "react";
-import { graphql } from "gatsby";
 import { css } from "@emotion/core";
+import { Link, graphql } from "gatsby";
 import { rhythm } from "../utils/typography";
 import Layout from "../components/layout";
 
 export default ({ data }) => {
   console.log(data);
-  return (
-    <Layout>
+  return <Layout>
       <div>
-        <h1
-          css={css`
+        <h1 css={css`
             display: inline-block;
-            border-bottom: 1px solid;
-          `}
-        >
+            border-bottom: 1px solid;`}>
           Amazing Pandas Eating Things
         </h1>
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
-        {data.allMarkdownRemark.edges.map(({ node }) => (
+        {data.allMarkdownRemark.edges.map(({ node }) => 
           <div key={node.id}>
-            <h3
+            <Link 
+              to={node.fields.slug} 
               css={css`
-                margin-bottom: ${rhythm(1 / 4)};
+                text-decoration: none;
+                color: inherit;
               `}
             >
-              {node.frontmatter.title}{" "}
-              <span
-                css={css`
-                  color: #BBBBBB;
-                `}
-              >
-                — {node.frontmatter.date}
-              </span>
-            </h3>
-            <p>{node.excerpt}</p>
-          </div>
-        ))}
+              <h3 css={css`
+                  margin-bottom: ${rhythm(1 / 4)};`}>
+                {node.frontmatter.title} <span css={css`
+                    color: #bbbbbb;`}>
+                  — {node.frontmatter.date}
+                </span>
+              </h3>
+              <p>{node.excerpt}</p>
+            </Link>
+          </div>)}
       </div>
-    </Layout>
-  );
+    </Layout>;
 };
 
 export const query = graphql`
@@ -52,6 +47,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
           }
           excerpt
         }
